d77107cf6fc7cb69629c36f76a60961ef0e7b2c6
georgosjohn <GeorgosJohn@JohnDeere.com>
2020-11-13 15:41:19 -0600

    heroku upload

--- models/index.js
+++ models/index.js
@@ -1,19 +1,42 @@
 'use strict';
 
+const dotenv = require('dotenv');
 const fs = require('fs');
 const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
+const { Sequelize, DataTypes } = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
+
+const filebasename = path.basename(__filename);
 const db = {};
 
+// const fs = require('fs');
+// const path = require('path');
+// const Sequelize = require('sequelize');
+// const basename = path.basename(__filename);
+// const env = process.env.NODE_ENV || 'development';
+// const config = require(__dirname + '/../config/config.json')[env];
+// const db = {};
+
+dotenv.config()
+const { DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_FORCE_RESTART } = process.env;
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    define: {
+      underscored: true
+    }
+  })
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, config);
 }
+// let sequelize;
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
 
 fs
   .readdirSync(__dirname)
